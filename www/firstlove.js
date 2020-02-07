8291c6ae639a404095d68641f908513673140bfc
crapthings <crapthings@gmail.com>
2020-02-07 19:13:43 +0800

    add new fn

--- www/firstlove.js
+++ www/firstlove.js
@@ -1,11 +1,16 @@
-var exec = require('cordova/exec');
+var exec = require('cordova/exec')
 
-var PLUGIN_NAME = "firstlove"; // This is just for code completion uses.
+var PLUGIN_NAME = "firstlove" // This is just for code completion uses.
 
-var firstlove = function() {}; // This just makes it easier for us to export all of the functions at once.
+var firstlove = function() {} // This just makes it easier for us to export all of the functions at once.
 // All of your plugin functions go below this.
 // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
 firstlove.yeah = function(onSuccess, onError) {
-  exec(onSuccess, onError, PLUGIN_NAME, 'yeah', []);
-};
-module.exports = firstlove;
+  exec(onSuccess, onError, PLUGIN_NAME, 'yeah', [])
+}
+
+firstlove.test = function(onSuccess, onError) {
+  exec(onSuccess, onError, PLUGIN_NAME, 'test', [])
+}
+
+module.exports = firstlove
